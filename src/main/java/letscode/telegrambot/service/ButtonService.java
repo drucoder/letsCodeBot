4ae564fc67a051293aa388fafc3642d14f9d3fcf
Andrey <popov.a83@bk.ru>
2018-06-14 12:36:06 +0200

    complete answer list

--- src/main/java/letscode/telegrambot/service/ButtonService.java
+++ src/main/java/letscode/telegrambot/service/ButtonService.java
@@ -1,13 +1,11 @@
 package letscode.telegrambot.service;
 
 import letscode.telegrambot.domain.BotMessage;
-import letscode.telegrambot.domain.BotUser;
 import letscode.telegrambot.domain.Buttons;
 import letscode.telegrambot.repo.MessageRepo;
 import letscode.telegrambot.utils.MessageBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.CallbackQuery;
 import org.telegram.telegrambots.api.objects.Message;
 
@@ -22,24 +20,31 @@ import java.util.List;
 public class ButtonService {
 
     @Autowired
-    private MessageService messageService;
-
-    @Autowired
     private MessageRepo messageRepo;
 
-    private BotUser botUser;
+    @Autowired
+    private MessageService messageService;
 
+    private boolean isAnswer;
+
+    /**
+     * Отрабатываем команды кнопок меню
+     * @param message - принятое сообщение из чата
+     * @param text - текст( команда из чата)
+     */
     public void getData(Message message, String text) {
 
         Buttons button = Buttons.get(text);
         MessageBuilder messageBuilder = new MessageBuilder();
+
         switch (button) {
             case HELP:
 
                 break;
             case QUESTIONS_LIST:
-                List<BotMessage> botMessageList = messageRepo.findAllByDoneIsFalse();
-                messageBuilder.createQuestions(message,botMessageList);
+                isAnswer = false;
+                List<BotMessage> botMessageList = messageRepo.findAllByDoneIsFalseAndAnswerForNull();
+                messageBuilder.createMessageList(message,botMessageList,isAnswer);
                 break;
             case MY_QUESTIONS_LIST:
                 break;
@@ -49,20 +54,60 @@ public class ButtonService {
         }
 
     }
-    private int extractId(String text) {
-        int id = Integer.parseInt(text.substring(text.indexOf("#")+1,
-                text.indexOf(":")));
-        return id;
-    }
 
     public void getCall(CallbackQuery callbackQuery) {
         Buttons button = Buttons.get(callbackQuery.getData());
         MessageBuilder messageBuilder = new MessageBuilder();
+        BotMessage botMessage;
+
         switch (button) {
-            case OPEN_QUESTIONS:
-                BotMessage quest = messageRepo.findById(extractId(callbackQuery.getMessage().getText()));
-                messageBuilder.detailedQuestions(quest, callbackQuery);
+            case OPEN_QUESTIONS:    //Обрабатываем callBackQuerry - getQuest
+                isAnswer = false;
+                botMessage = messageRepo.findById(   //находим вопрос по id из callBackQuerry.getMessage.getText()
+                        extractId(
+                                callbackQuery
+                                        .getMessage()
+                                        .getText()
+                        )
+                );
+
+                long count = messageRepo.countAllByAnswerFor(botMessage);    //счетчик ответов.
+                messageBuilder.detailedQuestions(botMessage, callbackQuery, count,isAnswer); //
                 break;
+            case SET_DONE:
+                messageService.setDone(callbackQuery);
+                messageBuilder.createDynamicMessage(callbackQuery,"Ваш вопрос закрыт, не забудьте отблагадорить тех, кто принимал участие в решении вашей проблемы");
+                break;
+            case GET_ANSWER_LIST:
+                isAnswer = true;
+                List<BotMessage> answerList = messageRepo
+                        .findAllByAnswerFor(messageRepo
+                                .findById(
+                                        extractId(
+                                            callbackQuery
+                                                .getMessage()
+                                                .getText())
+                                )
+                        );
+                messageBuilder.createMessageList(callbackQuery.getMessage(),answerList,isAnswer);
+                break;
+            case GET_ANSWER:
+                isAnswer = true;
+                botMessage = messageRepo.findById(   //находим вопрос по id из callBackQuerry.getMessage.getText()
+                        extractId(
+                                callbackQuery
+                                        .getMessage()
+                                        .getText()
+                        )
+                );
+                messageBuilder.detailedQuestions(botMessage,callbackQuery,0, isAnswer);
+
         }
+    }
+
+    private int extractId(String text) {
+        int id = Integer.parseInt(text.substring(text.indexOf("#")+1,
+                text.indexOf(":")));
+        return id;
     }
 }
--- src/main/java/letscode/telegrambot/utils/MessageBuilder.java
+++ src/main/java/letscode/telegrambot/utils/MessageBuilder.java
@@ -3,54 +3,120 @@ package letscode.telegrambot.utils;
 import letscode.telegrambot.bot.LetsCodeBot;
 import letscode.telegrambot.config.KeyboardReply;
 import letscode.telegrambot.domain.BotMessage;
+import letscode.telegrambot.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.CallbackQuery;
 import org.telegram.telegrambots.api.objects.Message;
 
 import java.util.List;
 
-@Component
+/**
+ * Этот клас был создан из-за того, что у меня вывалилась ошибка цикла БИНОВ
+ */
+@Service
 public class MessageBuilder {
 
     @Autowired
     LetsCodeBot letsCodeBot;
 
-    public void createQuestions
-            (Message message, List<BotMessage> botMessageList) {
-        String getText;
+    @Autowired
+    MessageRepo messageRepo;
+
+    /**
+     * Метод создает сообщения списком.
+     * @param message - принятое сообщение из чата для распарсивания
+     * @param botMessageList - список сообщений
+     * @param isAnswer - так как этот метод используется для вывода сообщений вопросов и ответов,
+     *                 то при формировании шапки, и кнопок используется эта булевая переменная
+     */
+    public void createMessageList
+            (Message message, List<BotMessage> botMessageList, boolean isAnswer) {
+        String getText, title;
         SendMessage sendMessage = new SendMessage();
         KeyboardReply keyboardReply = new KeyboardReply();
         LetsCodeBot letsCodeBot = new LetsCodeBot();
-        for (BotMessage botMessage:botMessageList) {
-            if (botMessage.getText().length() > 50) {
+
+        for (BotMessage botMessage:botMessageList) {        //пробегаемся по списку botMessageList
+
+            if (botMessage.getText().length() > 50) {       // если сообщение длиннее 50 символов режем его
                 getText = botMessage.getText().substring(0, 50);
             } else {
                 getText = botMessage.getText();
             }
+
             sendMessage.setChatId(message.getChatId());
-            sendMessage.setText("<code>"+getText+"</code>\n" +
+
+            if (!isAnswer) {                                        //определяем, вопрос это или ответ.
+                title = "<b>Вопрос #"+botMessage.getId()+":</b>\n"; //шапка для вопроса
+            } else {
+                title = "<b>Ответ #"+botMessage.getId()+":</b>\n";  //шапка для ответа.
+            }
+
+            sendMessage.setText(title +
+                    "<code>"+getText+"</code>\n" +
                     "<b>Автор: "+botMessage.getFrom().getUserName()+"</b>");
-            sendMessage.enableHtml(true);
-            keyboardReply.addOpenButton(sendMessage);
-            letsCodeBot.send(sendMessage);
+
+            sendMessage.enableHtml(true);                           //устанавливаем HTML разметку в сообщение
+            keyboardReply.addOpenButton(sendMessage,isAnswer);      // устанавливаем клавиатуру для сообщения
+            letsCodeBot.send(sendMessage);                          // отправляем сообщение
         }
     }
 
-    public void detailedQuestions(BotMessage message, CallbackQuery callbackQuery) {
+    /**
+     * Метод отображает вопрос полностью.
+     * @param botMessage - вопрос из БД
+     * @param callbackQuery - перехваченный update из чата, который приходит в случае нажатия InlineKeyboardReply
+     * @param count - счетчик ответов.
+     * @param isAnswer
+     */
+    public void detailedQuestions(
+                                    BotMessage botMessage,
+                                    CallbackQuery callbackQuery,
+                                    long count,
+                                    boolean isAnswer) {
+        String botText, title;
         SendMessage sendMessage = new SendMessage();
         KeyboardReply keyboardReply = new KeyboardReply();
         LetsCodeBot letsCodeBot = new LetsCodeBot();
         sendMessage.setChatId(callbackQuery.getMessage().getChatId());
         sendMessage.enableHtml(true);
-        sendMessage.setText("<code>"+message.getText()+"</code>\n" +
-                "<b>Автор: "+message.getFrom().getUserName()+"</b>");
-        if (callbackQuery.getFrom().getId().equals(message.getFrom().getId())) {
-            keyboardReply.addButtonDone(sendMessage);
+
+        if (isAnswer) {
+            title = "<b>Ответ #" + botMessage.getId() + ":</b>\n";
+        } else {
+            title = "<b>Вопрос #" + botMessage.getId() + ":</b>\n";
         }
+        boolean enableAnswer = count > 0;
+        if (enableAnswer) {
+            botText = title +
+                    "<code>" + botMessage.getText() + "</code>\n" +
+                    "<b>Автор: " + botMessage.getFrom().getUserName() + "    \uD83D\uDCAC: " + count + "</b>";
+        } else {
+            botText = title +
+                    "<code>" + botMessage.getText() + "</code>\n" +
+                    "<b>Автор: " + botMessage.getFrom().getUserName() + "    \uD83D\uDCAC: " + count + "</b>";
+        }
+        sendMessage.setText(botText);
+        boolean isAuthor = callbackQuery.getFrom().getId().equals(botMessage.getFrom().getId()); // проверяем, автора сообщения и пользователя нажавшего кнопку
+            // проверяем есть ли ответы на этот вопрос
+            keyboardReply.addTwoLineKeyboard(sendMessage, isAuthor, enableAnswer, isAnswer);  //собираем клавиатуру
         letsCodeBot.send(sendMessage);
     }
 
+    /** TODO переделать метод
+     * Создает динамические сообщения для уведомления пользователей
+     * @param callbackQuery
+     * @param botText
+     */
+    public void createDynamicMessage(CallbackQuery callbackQuery, String botText) {
+        SendMessage sendMessage = new SendMessage();
+        LetsCodeBot letsCodeBot = new LetsCodeBot();
+        sendMessage.setChatId(callbackQuery.getMessage().getChatId());
+        sendMessage.enableHtml(true);
+        sendMessage.setText(botText);
+        letsCodeBot.send(sendMessage);
+    }
 
 }
